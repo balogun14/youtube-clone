4b3ad98e817367d3fd662655e7f8b895d24356d1
balogun14 <awwalbalogun06@gmail.com>
2024-01-28 20:10:53 +0100

    Add videoProcessing endpoint

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,9 +1,34 @@
 import express from "express";
-
+import ffmpeg from "fluent-ffmpeg";
 const app = express();
 const port = 3000;
-app.get("/", (req, res) => {
-  res.send("Hello World");
+
+app.post("/process-video", (req, res) => {
+  //Get path from input video from the request body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send("Bad request: Missing file path.");
+  }
+
+  try {
+    ffmpeg(inputFilePath)
+      .outputOption("-vf", "scale=-1:360")
+      .on("end", () => {
+        return res.status(200).send("Processing finished succesfully");
+      })
+      .on("error", (err) => {
+        throw new Error(
+          `An error occured while trying to convert video:${err.message}`
+        );
+      })
+      .save(outputFilePath); //Converting the resolution to 360p
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).send(`Internal Server error: ${error.message}`);
+    }
+  }
 });
 
 app.listen(port, () => {
