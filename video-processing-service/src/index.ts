a44269cb4622b9a0a1cb4d03610a218e311fbf22
balogun14 <awwalbalogun06@gmail.com>
2024-01-30 18:57:21 +0100

    Fix Bug

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,39 +1,33 @@
 import express from "express";
 import ffmpeg from "fluent-ffmpeg";
 const app = express();
-app.use(express.json);
+
+app.use(express.json());
+
 app.post("/process-video", (req, res) => {
-  //Get path from input video from the request body
-  const inputFilePath = req.body.inputFilePath;
-  const outputFilePath = req.body.outputFilePath;
+	//Get path from input video from the request body
+	const inputFilePath = req.body.inputFilePath;
+	const outputFilePath = req.body.outputFilePath;
+	console.log(inputFilePath);
 
-  if (!inputFilePath || !outputFilePath) {
-    res.status(400).send("Bad request: Missing file path.");
-  }
+	if (!inputFilePath || !outputFilePath) {
+		res.status(400).send("Bad request: Missing file path.");
+	}
 
-  try {
-    ffmpeg(inputFilePath)
-      .outputOption("-vf", "scale=-1:360")
-      .on("end", () => {
-        return res.status(200).send("Processing finished succesfully");
-      })
-      .on("error", (err) => {
-        throw new Error(
-          `An error occured while trying to convert video:${err.message}`
-        );
-      })
-      .save(outputFilePath); //Converting the resolution to 360p
-  } catch (error) {
-    if (error instanceof Error) {
-      res.status(500).send(`Internal Server error: ${error.message}`);
-    }
-  }
+	ffmpeg(inputFilePath)
+		.outputOption("-vf", "scale=-1:360")
+		.on("end", () => {
+			res.status(200).send("Processing finished succesfully");
+		})
+		.on("error", (err) => {
+			res.status(500).send(`Internal Server error: ${err.message}`);
+		})
+		.save(outputFilePath); //Converting the resolution to 360p
 });
 
-const port = process.env.PORt || 3000;
+const PORT: number = 3000;
+const HOST: string = process.env.HOST || "localhost";
 
-app.listen(port, () => {
-  console.log(
-    `Video processing service listening at https://localhost:${port}`
-  );
+app.listen(PORT, HOST, () => {
+	console.log(`Server is running on http://${HOST}:${PORT}`);
 });
